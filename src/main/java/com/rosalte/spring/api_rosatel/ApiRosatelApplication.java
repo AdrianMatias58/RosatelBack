db6425a1e3662e99ab69597b748af936854627f7
AdrianMatias58 <matihu.919adrian@gmail.com>
2026-05-21 14:20:24 -0500

    primer commit

---
+++ src/main/java/com/rosalte/spring/api_rosatel/ApiRosatelApplication.java
@@ -0,0 +1,13 @@
+package com.rosalte.spring.api_rosatel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRosatelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRosatelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rosalte/spring/api_rosatel/ApiRosatelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosalte.spring.api_rosatel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRosatelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
